f68448fe0c51ea740600e55241cad55c09cae919
tazz <tazz@tazz.cloud>
2025-12-15 22:29:35 -0500

    refactor: 🔨 code cleanup

--- plugin.py
+++ plugin.py
@@ -1,17 +1,1 @@
-import subprocess, dotbot
-from src import YaPlugin, bcolors
-
-__version__ = "0.0.1"
-
-
-class DotbotPlugin(dotbot.Plugin):
-    def can_handle(self, directive):
-        valid = directive == "plugin"
-        if not valid:
-            self._log.debug(f"The plugin doesn't support the `{directive}` directive")
-        return valid
-
-    def handle(self, directive, data):
-        if not self.can_handle(directive):
-            return False
-        pass  # TODO(@s0cks): implement
+from src import DotbotPlugin, __version__
